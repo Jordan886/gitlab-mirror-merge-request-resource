dd418bab3a8e0cfd8ebcb34b7519d8cf07574f62
Giordano Corradini <a>
2021-05-14 18:31:41 +0200

    initial refactor to replicate merge requests

--- src/function/check.py
+++ src/function/check.py
@@ -2,15 +2,30 @@
 import sys
 import requests
 import json
+import datetime
+import urllib.parse
 from pprint import pprint
 from load_json import LoadJson
 
-check_response = []
 # Load config 
 config = LoadJson(sys.stdin)
+
+# Only merge request of this day will be returned
+# yesterday = datetime.date.today() - datetime.timedelta(days=1)
+# print(yesterday.day)
+
+project_id = urllib.parse.quote(config.project,safe='')
+
+current_day = datetime.datetime.utcnow().replace(day=1, hour=0, minute=0, second=0, microsecond=0).isoformat()
+
 # Prepare GitlabRequest
-params = '?' + 'scope=all' + '&view=simple' + '&state=opened'
-url = 'https://' + config.api_url + '/api/v4/merge_requests' + params
+params = ('?' + 
+          'scope=all' + 
+          '&state=merged' +
+          f'&target_branch={config.source_branch}'
+          f'&updated_after={current_day}'
+          )
+url = 'https://' + config.api_url + '/api/v4/projects/' + project_id + '/merge_requests' + params
 headers = {
   'Authorization': f'Bearer {config.access_token}'
 }
@@ -23,5 +38,20 @@ if response.status_code != 200 :
   print(f'Response: {response.content}')
   raise  Exception(f'Gitlab server returned error')  
 
-print(json.dumps(check_response))
+# print(json.dumps(check_response))
 
+json_response = response.json()
+
+wanted_keys=['id','title','project_id','source_branch']
+check_response = []
+
+for merge_request in json_response:
+  simple_merge={}
+  for key,value in merge_request.items():
+    if key in wanted_keys:
+      simple_merge[key] = value
+  check_response.append(simple_merge)
+
+print(check_response)
+
+# print(json.dumps(check_response))
--- src/function/out.py
+++ src/function/out.py
@@ -12,7 +12,7 @@ out_response = []
 config = LoadJson(sys.stdin)
 
 #If project name contains spaces or special character
-project_id = urllib.parse.quote(config.project)
+project_id = urllib.parse.quote(config.project,safe='')
 
 # Title will have datestamp at the beginning to avoid conflicts
 title_suffix = datetime.now().isoformat(timespec='seconds')
@@ -21,23 +21,22 @@ title_complete = title_suffix + '' + config.title
 # Prepare GitlabRequest
 params = ''
 
-url = 'https://' + config.api_url + '/api/v4/projects' + project_id + 'merge_requests' +  params
+url = 'https://' + config.api_url + '/api/v4/projects/' + project_id + '/merge_requests' +  params
 headers = {
   'Authorization': f'Bearer {config.access_token}'
 }
 payload={
-  "id": project_id,
   "source_branch": config.source_branch,
   "target_branch": config.target_branch,
-  "title": config.title
+  "title": title_complete
 }
 
 print(url,headers,payload)
 
-# response = requests.request("POST", url, headers=headers, data=payload)
+response = requests.request("POST", url, headers=headers, data=payload)
 
 
-# if response.status_code != 200 :
-#   print(f'StatusCode: {response.status_code}')
-#   print(f'Response: {response.content}')
-#   raise  Exception(f'Gitlab server returned error')
+if response.status_code not in (200,201) :
+  print(f'StatusCode: {response.status_code}')
+  print(f'Response: {response.content}')
+  raise  Exception(f'Gitlab server returned error')
